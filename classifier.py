10fe4cbe5a096fb916a831e8d44541eb6989778b
Alex Charters <alexcharters45@gmail.com>
2019-10-05 13:33:53 -0600

    added file structure and basic imports

---
+++ classifier.py
@@ -0,0 +1,7 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Concatenate
+from keras.models import Model
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.layers.core import Activation, Flatten, Dropout, Dense
+
