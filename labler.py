e41375f1dd4722ae1defffff1f00160f8ca3a5b4
Ted <edwardjohng@gmail.com>
2019-10-06 08:30:22 -0600

    Merge branch 'master' of https://github.com/alexCharters/Diggatron_2000
    # Conflicts:
    #	DataGenerator.py
    
    made a labler
    
    IT WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- labler.py
+++ labler.py
@@ -2,32 +2,30 @@ from keras.models import Model, model_from_json
 from keras.preprocessing.image import img_to_array, load_img
 import pandas as pd
 import numpy as np
+import os
 
 #make a model and load weights
 
 model = model_from_json(open('model.json').read())
+model.load_weights('weights-improvement-99-0.91.hdf5')
 
 #load in a test image and it's parameters
 metaIn = pd.read_csv("specs/test_metadata.csv", index_col="Name")
-img_name = 'bp1-01-001.png'
-img = load_img("specs/test/" + img_name)
-img_array = img_to_array(img)
-#need to either load in or calculate the other usful data for waveform
+for root, dirs, files in os.walk("./specs/test"):
+    for file in files:
+        img_name = file
+        img = load_img("specs/test/" + img_name)
+        img_array = img_to_array(img)
+        #need to either load in or calculate the other usful data for waveform
 
-row = metaIn.loc[img_name, : ]
+        row = metaIn.loc[img_name, : ]
 
-#load in the weights from a trained set
+        # evaluate it with the model
 
-# model = Model.load_model("weights-improvement-99-0.91.hdf5")
+        pred = model.predict([np.expand_dims(img_array, axis=0), row.iloc[1:4].values.reshape(1, 3)])
 
-
-
-
-# evaluate it with the model
-
-pred = model.predict(np.array([np.expand_dims(img_array, axis=0), row.iloc[1:4].values]))
-
-print(pred)
+        print(file)
+        print(pred)
 
 
 
