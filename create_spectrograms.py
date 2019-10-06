6aa3975bd52302460dbb575a4d6987b77c1659ef
Jackson Dean <jacksondean17@gmail.com>
2019-10-05 20:44:22 -0600

    create_spectrograms.py completed. All spectrograms generated and added to /specs

--- create_spectrograms.py
+++ create_spectrograms.py
@@ -5,19 +5,19 @@ import os
 from PIL import Image
 """create_data.py: Converts all of the .wav files into spectrograms and finds other features for each clip"""
 
-n = 1
-for root, dirs, files in os.walk("./sounds/"):
+# n = 1
+for root, dirs, files in os.walk("./all_samples/"):
     for f in files:
         # load audio file
-        data, sampling_rate = librosa.load("./sounds/"+f)
+        data, sampling_rate = librosa.load("./all_samples/"+f)
         # create figure, remove borders
-        fig = plt.figure(n, figsize=(12, 4))
-        n = n+1
+        fig = plt.figure(figsize=(12, 4))
+        # n = n+1
         ax = fig.add_axes([0, 0, 1, 1])
         ax.axis('off')
         # plt.title(f)
         S = librosa.feature.melspectrogram(y=data, sr=sampling_rate)
-        librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='log', fmax=8000, x_axis='time')
+        librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', fmax=8000, x_axis='time')
         spec_file_name = "./specs/" + f[:-4] + ".png"
 
         plt.savefig(spec_file_name)
@@ -25,3 +25,4 @@ for root, dirs, files in os.walk("./sounds/"):
         resolution = (240, 160)
         img = img.resize(resolution)
         img.save(spec_file_name)
+        plt.close()
